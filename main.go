42f5fa047952e31da3e5bbdb053e309d5608b037
edwinavalos <edwin.a.avalos@gmail.com>
2023-06-02 23:16:39 -0500

    add comments and relay code

--- main.go
+++ main.go
@@ -7,14 +7,24 @@ import (
 
 func main() {
 	log.Println("Opening rpio")
-	err := rpio.Open() // Open up our access to the GPIO pins (requires sudo)
+	err := rpio.Open() // Open access to the GPIO
 	if err != nil {
 		log.Panicf("could not open rpio: %s", err)
 	}
-	defer rpio.Close()
+	defer rpio.Close() // Close the connection once we are done with this function
 
-	relay1Pin := rpio.Pin(18)
-	relay1Pin.Output()
-	relay1Pin.High()
+	relay1Pin := 18 // This is the pin that we connected to our first relay
+	log.Printf("connecting to pin %d\n", relay1Pin)
+	relay1 := rpio.Pin(18) // instantiate the pin
 
+	state := relay1.Read() // Read its current status
+	if state == rpio.Low {
+		log.Println("relay is currently 'off'")
+	} else {
+		log.Println("relay is currently 'on'")
+	}
+
+	relay1.Output() // Prepare the pin to be written to
+	log.Println("toggling the relay")
+	relay1.Toggle() // Toggle the relay, it switches from high to low and low to high
 }
