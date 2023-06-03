d50b6d1ca9ef9b5045446380d4fdc868165391ba
edwinavalos <edwin.a.avalos@gmail.com>
2023-06-02 23:07:40 -0500

    try this out

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/edwinavalos/go-pi-relay-example
 
 go 1.19
+
+require (
+	github.com/stianeikeland/go-rpio/v4 v4.6.0
+	periph.io/x/conn/v3 v3.7.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/stianeikeland/go-rpio/v4"
+	"log"
+)
+
 func main() {
+	log.Println("Opening rpio")
+	err := rpio.Open() // Open up our access to the GPIO pins (requires sudo)
+	if err != nil {
+		log.Panicf("could not open rpio: %s", err)
+	}
+	defer rpio.Close()
+
+	relay1Pin := rpio.Pin(18)
+	relay1Pin.Output()
+	relay1Pin.High()
 
 }
